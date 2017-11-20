2404020734ce8aa9c864037cc0d337d9572e3426
Matan Noam Shavit <matannoam@gmail.com>
2017-11-20 17:55:01 -0500

    additional tests for too few arguements

--- tests/TestQuotes.php
+++ tests/TestQuotes.php
@@ -8,10 +8,26 @@ final class QuotesTests extends TestCase
     {
         $author = "Cato";
         $text = "Grasp the subject, the words will follow.";
-
+        
         $quote = new Quote($text, $author);
 
         $this->assertEquals($quote->author(), $author);
         $this->assertEquals($quote->text(), $text);
     }
+
+     public function testCannotCreateWithoutArguments()
+     {
+        $this->expectException(ArgumentCountError::class);
+
+        new Quote();
+     }
+
+     public function testCannotCreateWithoutAuthor()
+     {
+        $text = "Grasp the subject, the words will follow.";
+
+        $this->expectException(ArgumentCountError::class);
+
+        $quote = new Quote($text);
+     }
 }
